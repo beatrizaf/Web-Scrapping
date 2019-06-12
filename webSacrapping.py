22eabe1bf95ba05f701d89012474670f6d496715
Ana Beatriz de Araujo Farias <33091171+beatrizaraujo1@users.noreply.github.com>
2019-06-12 13:49:25 -0300

    Update webSacrapping.py

--- webSacrapping.py
+++ webSacrapping.py
@@ -1,1 +1,10 @@
 #Projeto de MINERAÇÃO DE DADOS - Algoritmos e Lógica de Programação 2019.1
+#coding: utf-8
+import json
+import os
+import 
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
+
+
