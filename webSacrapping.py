3109d214d585565897f6bd72a4f674916834f92c
Ana Beatriz de Araujo Farias <33091171+beatrizaraujo1@users.noreply.github.com>
2019-06-18 08:50:14 -0300

    Update webSacrapping.py

--- webSacrapping.py
+++ webSacrapping.py
@@ -1,10 +1,17 @@
 #Projeto de MINERAÇÃO DE DADOS - Algoritmos e Lógica de Programação 2019.1
 #coding: utf-8
+#coding: utf-8
 import json
 import os
-import 
 import urllib.request
 import requests
 from bs4 import BeautifulSoup
+import seaborn as sns
+
+def abrir():
+    with open('analise.json') as f:    
+        data = json.load(f)
+    f.close()
+    return data 
 
 
