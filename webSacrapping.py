56e4bd6c34e5f89f5a992f71dfabec08e1d527d9
Ana Beatriz de Araujo Farias <33091171+beatrizaraujo1@users.noreply.github.com>
2019-06-19 10:03:19 -0300

    Update webSacrapping.py

--- webSacrapping.py
+++ webSacrapping.py
@@ -1,21 +1,43 @@
-#Projeto de MINERAÇÃO DE DADOS - Algoritmos e Lógica de Programação 2019.1
 #coding: utf-8
-#coding: utf-8
-import json
-import os
-import urllib.request
-import requests
-from bs4 import BeautifulSoup
-import seaborn as sns
+import json #Usamos o Json para manipulação de arquivos json
+import os # Usamos o OS para adiquirir informações a respeito do SO e adequar o funcionamento independentemente do SO em uso 
+import urllib.request #Usamos a urllib.request
+import requests #Usamos os requests para abrir as URLs em questão
+from bs4 import BeautifulSoup #Usamos o Beautiful Soup para fazer a extração dos dados
 
-busca = input("Digite o que deseja pesquisar na web:")
-json.dumps(busca) #converte para json
+"""
+#Gerar histograma e nuvem de palavra
+import seaborn as sns #Usamos para gerar o gráfico
+OBSERVAÇÃO: A partir desse trecho de código, importaremos as bibliotecas fundamentais  para a geração da Nuvem de Palavra
+import re
+from worldcloud import WorldCloud, STOPWORDS, ImageColorGenerator #OBS: 
+from os import path
+import matplotlib.pyplot as plt
+"""
 
+listadearquivos = ["armazena.txt"]
+l1 = []
+l2 = []
+
+#função criada para abertura do arquivo JSON
 def abrir():
-    with open('analise.json') as f: #abertura do arquivo json
-        data = json.load(f) #convertendo para um objeto Python
-        
+    with open('analise.json', "r") as f:    
+        dado = json.load(f)
     f.close()
-    return data 
+    print (dado)
+    return dado
 
+""" Sabendo que essa variável "data" vai me retornar o 
+"""
+#função criada para a inserção de palavra pelo o usuário e para a validação
 
+def receber_e_validar():
+    abrir = True
+    while abrir == True:
+        palavra = input("Digite a sua pesquisa: ").lower() #Usando o lower par deixar as entradas como minúsculo, por padrão para cada entrada
+        if palavra in dado:
+            lista = dado[palavra]
+            abrir = False
+            return palavra
+        else:
+            print("A expressão procurada é inexistente")
