024ca17ca4878fae2077a7bad6377c9d1348f893
Ana Beatriz de Araujo Farias <33091171+beatrizaraujo1@users.noreply.github.com>
2019-06-25 15:17:50 -0300

    Update webSacrapping.py

--- webSacrapping.py
+++ webSacrapping.py
@@ -1,43 +1,118 @@
-#coding: utf-8
-import json #Usamos o Json para manipulação de arquivos json
-import os # Usamos o OS para adiquirir informações a respeito do SO e adequar o funcionamento independentemente do SO em uso 
-import urllib.request #Usamos a urllib.request
-import requests #Usamos os requests para abrir as URLs em questão
-from bs4 import BeautifulSoup #Usamos o Beautiful Soup para fazer a extração dos dados
+# ! -*- encoding:utf-8 -*-
+# !/usr/bin/python
+import json  # Importa um arquivo com um formato de intercâmbio de dados leve inspirado por JavaScript
+import os  # Dentre tantas funcionalidades, este módulo fornece também uma maneira portátil de usar a funcionalidade dependente do sistema operacional
+import io
+import urllib2
+from pylab import *  # Dentre suas funcionalidades, assumirá a função de gerar os gráficos
+import requests # Assumirá a função de definir funções e classes que ajudam na abertura de URLs
+from bs4 import BeautifulSoup  # Essa biblioteca possibilita fazer a extração dos dados de uma forma mais prática
 
-"""
-#Gerar histograma e nuvem de palavra
-import seaborn as sns #Usamos para gerar o gráfico
-OBSERVAÇÃO: A partir desse trecho de código, importaremos as bibliotecas fundamentais  para a geração da Nuvem de Palavra
-import re
-from worldcloud import WorldCloud, STOPWORDS, ImageColorGenerator #OBS: 
-from os import path
-import matplotlib.pyplot as plt
-"""
 
-listadearquivos = ["armazena.txt"]
-l1 = []
-l2 = []
 
-#função criada para abertura do arquivo JSON
-def abrir():
-    with open('analise.json', "r") as f:    
-        dado = json.load(f)
-    f.close()
-    print (dado)
-    return dado
+# LISTAS CRIADAS PARA ARMAZENAR A EXTRAÇÃO DAS URL's
+arquivos = ["armazena_extracao.txt"]
+lista1 = []  # NO DECORRER DO CÓDIGO, MOSTRAREI A UTILIDADE DESSA LISTA
+lista2 = []  # NO DECORRER DO CÓDIGO, MOSTRAREI A UTILIDADE DESSA LISTA
 
-""" Sabendo que essa variável "data" vai me retornar o 
-"""
-#função criada para a inserção de palavra pelo o usuário e para a validação
 
-def receber_e_validar():
+# ABERTURA DO ARQUIVO JSON
+def abertura():
+    with io.open("analise.json", "r", encoding= 'utf-8') as data_file:
+        data = json.load(data_file)
+        print(data)
+    data_file.close()
+    return data
+
+
+# ANÁLISE DE INSERÇÃO DO USUÁRIO
+def analise(data):
     abrir = True
     while abrir == True:
-        palavra = input("Digite a sua pesquisa: ").lower() #Usando o lower par deixar as entradas como minúsculo, por padrão para cada entrada
-        if palavra in dado:
-            lista = dado[palavra]
+
+        letter = input("Informe:")  # RECEBE A PALAVRA DA ENTRADA PADRÃO E PÕES EM MINUSCULO PARA PADRONIZAR A LEITURA
+
+        if letter in data:
+            lista = data[letter]
+            print(lista)
+
             abrir = False
-            return palavra
+            return letter
         else:
-            print("A expressão procurada é inexistente")
+            print('Palavra inválida')
+
+
+# PROCURA NO ARQUIVO JSON AS NOTÍCIAS
+def procura_armazenamento(data, letter):
+    # VARIÁVEL "STRINGS" ARMAZENA A LISTA REFERENTE A CHAVE DIGITADA PELO USUÁRIO
+    # Ex.: "nome" : ["Martin", "Luther", "King", "Malcolm"] -> ESSA LISTA SERÁ ARMANEZADA EM "STRINGS"
+    strings = data[letter]
+    # VARIÁVEL CRIARÁ UM VETOR E ORDENARÁ A POSIÇÃO DE CADA UM
+    # Ex.: ["Martin", "Luther", "King", "Malcolm"], NESSA SITUAÇÃO TEMOS 4 ELEMENTOS
+    pos = arange(len(data[letter]))
+    # ABRIRÁ O DIRETÓRIO E ANALISARÁ TODOS OS ARQUIVOS INSERIDOS NELE.
+    files = [f for f in os.listdir(".") if f.startswith("armazena_extracao.txt") and f.endswith('.txt')]
+    # 1º FOR TERÁ UTILIDADE DE PERCORRER TODOS OS TXT'S ALOCADOS NO DIRETÓRIO
+    for filename in files:
+        # ESSAS VARIÁVEIS AQUI, ASSUMIRAM O PAPEL DE ZERAR QUANDO FOR FEITA UMA ITERAÇÃO
+        lista1 = []
+        lista2 = []
+        with open(filename, encoding='utf-8', mode='r') as f:
+            arquivo = f.read()
+
+            # 2º FARÁ A COMPARAÇÃO DE CADA ELEMENTO DA VARIÁVEL "STRINGS" COM CADA ELEMENTO DO ARQUIVO TXT
+            for s in strings:
+                print (filename, s, arquivo.count(s))
+                # LISTA1 ARMANEZA OS ELEMENTOS EM COMUM
+                lista1.append(s)
+                # LISTA2 ARMAZENA A INCIDÊNCIA DAS PALAVRAS NO TXT
+                lista2.append(arquivo.count(s))
+        f.close()
+    return lista1, lista2, pos
+
+
+def grafico(lista1, lista2, pos):
+    # APÓS A PROCURA DO JSON NO ARQUIVO GERAREMOS O GRÁFICO(HISTOGRAMA)
+    bar(pos, lista2, align='center', color='orange')
+    xticks(pos, lista1)
+    plt.title("ScrapingWeb")
+
+    show()
+    print(len(lista1))
+    print(lista2)
+
+
+# FUNÇÃO COM O PROPÓSITO DE FAZER O SCRAPINGWEB
+def url_search():
+    try:
+        # ABRE O ARQUIVO ONDE HÁ AS URL's PRÉ DEFENIDAS E AS DECODIFICA PARA A LEITURA DO PYTHON
+        with io.open("Exemplo.txt", encoding='utf-8', mode='r') as data_file:
+            # INICIALIZA A VARIAVEL PARA INSERÇÃO DOS TEXTOS LIDOS NAS URL's
+            soup_text = ""
+            for line in data_file:
+                html_content = urllib2.urlopen(line)
+                soup = BeautifulSoup(html_content, "lxml")
+                soup_text += soup.get_text()
+
+        data_file.close()
+
+        # ABRE ARQUIVO ONDE FORAM ARMAZENADOS OS TEXTOS DAS URL's
+        with io.open("armazena_extracao.txt", mode='a+') as out:
+            out.write(soup_text)
+            # print(out.write)
+
+        out.close()
+
+    except ValueError:
+        print("Fodeu")
+
+
+
+
+url_search()
+# FOR UTILIZADO PARA LER 3  VEZES O ARQUIVO "armazena_extracao.txt" COM  INSERÇÕES DIFERENTE DO USUÁRIO
+for i in range(6):
+    retorno = abertura()
+    palavra = analise(retorno)
+    lista1, lista2, pos = procura_armazenamento(retorno, palavra)
+    grafico(lista1, lista2, pos)
